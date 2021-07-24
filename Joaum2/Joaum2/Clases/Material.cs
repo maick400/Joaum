20554124eabd28f904948d3dda85e958e5f9f394
maick400 <69704088+maick400@users.noreply.github.com>
2021-07-24 14:58:26 -0500

    Add files via upload

---
+++ Joaum2/Joaum2/Clases/Material.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Joaum2.Data;
+
+namespace Joaum2.Clases
+{
+    class Material
+    {
+        Conexion conexion;
+        int id;
+        string material;
+
+        public string MATERIAL { get => material; set => material = value; }
+        public int ID { get => id; set => id = value; }
+
+
+
+        public DataTable Listar()
+        {
+            conexion = new Conexion();
+            return conexion.listarAny("ListarMaterial");
+
+        }
+
+
+    }
+}
