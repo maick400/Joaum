deb28cfe0ea72e04d96850761b5fcedc64fc1ccd
maick400 <69704088+maick400@users.noreply.github.com>
2021-07-25 13:58:31 -0500

    Add files via upload

---
+++ Clases/Categoria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Joaum2.Data;
+
+namespace Joaum2.Clases
+{
+    class Categoria
+    {
+        int Id;
+        string nombre;
+
+        public int Id1 { get => Id; set => Id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        Conexion Conex;
+
+
+        public DataTable ListarCategorias()
+        {
+            Conex = new Conexion();
+            return Conex.listarAny("ListarCategoria");
+        }
+
+
+
+
+
+    }
+
+   
+
+
+}
