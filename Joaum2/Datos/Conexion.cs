03da4830a6ecc6143629ca851b2ad1b20e1a4bfa
maick400 <69704088+maick400@users.noreply.github.com>
2021-07-26 09:48:36 -0500

    Mensaje 1

--- Joaum2/Datos/Conexion.cs
+++ Joaum2/Datos/Conexion.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -53,3 +54,60 @@ namespace Joaum2.Data
 
 
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Joaum2.Data
+{
+    public class Conexion
+    {
+
+      private  SqlConnection cn = new SqlConnection("Data Source=SQL5050.site4now.net;Initial Catalog=db_a77d2a_joaum;User Id=db_a77d2a_joaum_admin;Password=4006381172806Mm.");
+        SqlCommand cmd;
+
+    public DataTable listar(SqlCommand cmd)
+        {
+            cn.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = cn;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cn.Close();
+            return dt;
+
+
+
+        }
+
+  
+
+        public DataTable listarAny(string name)
+        {
+            cn.Open();
+            cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = name;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cn.Close();
+            return dt;
+           
+            
+        }
+
+    }
+
+
+}
+>>>>>>> 6c7c130917faa7710f4b8a49a98320af5cae0591
